55045e62a2dbe5450198ceb4f5384c182c19e086
longtkhd <longtkhd2709@gmail.com>
2020-09-10 17:36:33 +0700

    setting router

--- review-app/src/App.tsx
+++ review-app/src/App.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
+import history from './history';
+import Home from './container/Home/Home'
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      HOME
-     
+      <Router history={history}>
+        <div>
+          {/* <Header></Header> */}
+          <React.Fragment>
+            <Switch>
+              <Route path="/" exact component={Home} />
+
+
+
+            </Switch>
+          </React.Fragment>
+        </div>
+      </Router>
+
     </div>
   );
 }
---
+++ review-app/src/container/Home/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            this is the homepage
+        </div>
+    );
+}
+
+export default Home;
